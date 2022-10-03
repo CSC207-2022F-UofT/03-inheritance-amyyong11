39463a400c1ba7382f36719573c2ae868fc1b734
Amy <amy.yong11@yahoo.com>
2022-10-03 10:27:13 -0400

    Completed git exercise 03 - inheritance

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -5,6 +5,8 @@
  * 1. Introduction to Java helpful.
  */
 
+import java.sql.Array;
+
 public abstract class Bag {
     /*
      * TODO: Create the following private instance variables
@@ -13,6 +15,10 @@ public abstract class Bag {
      *       - an int named capacity
      *       - an array of Strings named contents
      */
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
 
 
@@ -26,6 +32,12 @@ public abstract class Bag {
      * be empty (e.g. numberOfContents is 0 and an empty String array for
      * its contents.)
      */
+    public Bag(String color, int capacity){
+        this.color = color;
+        this.capacity = capacity;
+        this.numberOfContents = 0;
+        this.contents = new String[capacity];
+    }
 
 
 
@@ -37,6 +49,17 @@ public abstract class Bag {
      *           - getNumberOfContents
      *           - getCapacity
      */
+    public String getColor(){
+        return this.color;
+    }
+
+    public int getNumberOfContents(){
+        return this.numberOfContents;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
 
 
 
@@ -45,6 +68,9 @@ public abstract class Bag {
      * TODO: Create a setter function called setColor which sets the
      *       color of this bag to the given color.
      */
+    public void setColor(String color){
+        this.color = color;
+    }
 
 
 
@@ -60,9 +86,18 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
-
-
-
+    public boolean addItem(String item){
+        if (this.numberOfContents < this.capacity){
+            int i = 0;
+            while (this.contents[i] != null){
+                i++;
+            }
+            this.contents[i] = item;
+            this.numberOfContents++;
+            return true;
+        }
+        return false;
+    }
 
 
     /**
@@ -75,6 +110,15 @@ public abstract class Bag {
      *
      * @return
      */
+    public String popItem(){
+        if (this.numberOfContents > 0){
+            String poppedItem = this.contents[this.numberOfContents - 1];
+            this.contents[this.numberOfContents - 1] = null;
+            this.numberOfContents--;
+            return poppedItem;
+        }
+        return null;
+    }
 
 
 
@@ -87,6 +131,12 @@ public abstract class Bag {
      */
     public void increaseCapacity(int n) {
         // TODO: Implement this method.
+        String[] holder = new String[this.getCapacity() + n];
+        for (int i = 0; i < this.getNumberOfContents(); i++){
+            holder[i] = this.contents[i];
+        }
+        this.contents = holder;
+        this.capacity += n;
 
     }
 
